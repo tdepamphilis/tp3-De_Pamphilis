46127d49fd7e96047d34ccd0a50d06e6381f0f53
tdepamphilis <62816494+tdepamphilis@users.noreply.github.com>
2020-05-20 14:17:18 -0300

    Add Frontend
    Business project needs the App.config file on the Frontend project

---
+++ TP3-DE_PAMPHILIS/FrontEnd/NuevaCategoria.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace FrontEnd
+{
+    partial class NuevaCategoria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button_Aceptar = new System.Windows.Forms.Button();
+            this.button_Cancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(75, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(198, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button_Aceptar
+            // 
+            this.button_Aceptar.Location = new System.Drawing.Point(160, 73);
+            this.button_Aceptar.Name = "button_Aceptar";
+            this.button_Aceptar.Size = new System.Drawing.Size(113, 28);
+            this.button_Aceptar.TabIndex = 1;
+            this.button_Aceptar.Text = "Aceptar";
+            this.button_Aceptar.UseVisualStyleBackColor = true;
+            this.button_Aceptar.Click += new System.EventHandler(this.button_Aceptar_Click);
+            // 
+            // button_Cancel
+            // 
+            this.button_Cancel.Location = new System.Drawing.Point(32, 73);
+            this.button_Cancel.Name = "button_Cancel";
+            this.button_Cancel.Size = new System.Drawing.Size(104, 28);
+            this.button_Cancel.TabIndex = 2;
+            this.button_Cancel.Text = "Cancelar";
+            this.button_Cancel.UseVisualStyleBackColor = true;
+            this.button_Cancel.Click += new System.EventHandler(this.button_Cancel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "NOMBRE:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // NuevaCategoria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 125);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_Cancel);
+            this.Controls.Add(this.button_Aceptar);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "NuevaCategoria";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button_Aceptar;
+        private System.Windows.Forms.Button button_Cancel;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ TP3-DE_PAMPHILIS/FrontEnd/NuevaCategoria.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Business;
+using Dominio;
+
+namespace FrontEnd
+{
+    public partial class NuevaCategoria : Form
+    {
+        int tipo;
+        public NuevaCategoria(int tipo)
+        {
+            this.tipo = tipo;
+            if (tipo == 0)
+                this.Text = "Nueva categoria";
+            else if (tipo == 1)
+                this.Text = "Nueva marca";
+            InitializeComponent();
+        }
+
+
+        private void button_Aceptar_Click(object sender, EventArgs e)
+        {
+            aceptar();
+
+        }
+        private void button_Cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        //--------VALIDACIONES-------------------
+
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (textBox1.Text == "" && e.KeyChar == 32)
+                e.Handled = true;
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+      
+        //------------PROCESOS-----------------------
+        private void aceptar()
+        {
+            CategoriaBusiness categoriaBusiness = new CategoriaBusiness();
+            MarcaBusiness marcaBusiness = new MarcaBusiness();
+            if (textBox1.Text != "")
+            {
+                if (tipo == 0)
+                    categoriaBusiness.add(textBox1.Text);
+                else if (tipo == 1)
+                    marcaBusiness.add(textBox1.Text);
+
+                this.Close();
+            }
+            else
+                MessageBox.Show("Agregue nombre");
+        
+        
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+}
